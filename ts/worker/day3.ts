dbc9400afc145c090e8035e236b046b0b7c42344
Alex Eckhart <eckhartalex@gmail.com>
2020-12-05 01:13:58 -0700

    Day3

---
+++ ts/worker/day3.ts
@@ -0,0 +1,26 @@
+import { AdventOutput } from '../common/common';
+
+function calcTrees(input: readonly string[], slopex: number, slopey = 1): number {
+    let column_count = input[0].length;
+    let row_count = input.length;
+    let x = 0;
+    let trees = 0;
+    for (let y = 0; y < row_count; y += slopey) {
+        if (input[y][x] === '#') {
+            trees++;
+        }
+        x += slopex;
+        x %= column_count;
+    }
+    return trees;
+}
+
+export default function (input: readonly string[]): AdventOutput {
+    let column_count = input[0].length;
+    let row_count = input.length;
+    let y = 0;
+    let part1 = calcTrees(input, 3);
+    let slopes: [number, number?][] = [[1], [3], [5], [7], [1, 2]];
+    let part2 = slopes.reduce((acc, val) => calcTrees(input, val[0], val[1]) * acc, 1);
+    return {part1: String(part1), part2: String(part2)};
+}
